02fe747fd014068ade7bbbbe09058f2d1f0ca684
Sunny Lam <sunny.lam@gmail.com>
2018-02-14 17:51:09 -0500

    revised readme

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# add_bullet_wiki_020518_1
+# Adding Bullets to a Wiki
+
+add_bullet_wiki_020518_1.py
+
+adds Wiki bullet points to the start of each line of text on the clipboard
+
+1.  Paste text from the clipboard
+2.  Do something to it
+3.  Copy the new text to the clipboard
+
+good program for understanding how to use the clipboard, and alter text with Python programs
--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ except ImportError:
 	from distutils.core import setup
 
 config = {
-	'description': 'My Project',
-	'author': 'My Name',
-	'url': 'URL to get it at',
-	'download_url': 'Where to download it',
-	'author_email': 'My email',
+	'description': 'Add Bullets to Wiki',
+	'author': 'Sunny Lam',
+	'url': 'https://github.com/sunnylam13/add_bullet_wiki_020518_1',
+	'download_url': 'https://github.com/sunnylam13/add_bullet_wiki_020518_1',
+	'author_email': 'sunny.lam@gmail.com',
 	'version': '0.1',
 	'install_requires': ['nose'],
 	'packages': ['NAME'],
 	'scripts': [],
-	'name': 'projectname'
+	'name': 'add_bullet_wiki_020518_1'
 }
 
 setup(**config)
